44bd4d4850edc060b6f4e579cb0d3c95887c6747
maude-atn <127099931+maude-atn@users.noreply.github.com>
2024-07-08 09:23:52 +0200

    Add files via upload

---
+++ Classe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Classe
+    {
+        public string nomClasse { get; private set; }
+
+        public List<Eleve> eleves = new List<Eleve>();
+        public List<string> matieres = new List<string>();
+        public Classe(string c)
+        {
+            nomClasse = c;
+        }
+
+        public void ajouterEleve(string p, string n)
+        {
+            eleves.Add(new Eleve(p, n));
+        }
+        public void ajouterMatiere(string m)
+        {
+            matieres.Add(m);
+        }
+        public double moyenneMatiere(int m)
+        {
+            double moyennec = 0;
+            foreach (Eleve e in eleves)
+            {
+                moyennec += e.moyenneMatiere(m);
+            }
+            return Math.Round(moyennec / eleves.Count, 2);
+        }
+        public double moyenneGeneral()
+        {
+            double moyennecg = 0;
+            for (int i = 1; i < matieres.Count; i++)
+            {
+                moyennecg += moyenneMatiere(i);
+            }
+            return Math.Round(moyennecg / matieres.Count, 2);
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Eleve
+    {
+        public string prenom { get; private set; }
+        public string nom { get; private set; }
+        public List<Note> note = new List<Note>();
+        public Eleve(string p, string n)
+        { 
+            prenom = p;
+            nom = n; 
+        }
+        public void ajouterNote(Note n)
+        {
+            note.Add(n);
+        }
+        public double moyenneMatiere(int m)
+        {
+            double moyenne = 0;
+            int compteur  = 0;
+            foreach (Note n in note)
+            {
+                if (n.matiere == m)
+                { 
+                    moyenne += n.note;
+                    compteur++;
+                }
+            }
+            return Math.Round(moyenne / compteur, 2);
+        }
+        public double moyenneGeneral()
+        {
+            double moyenneg = 0;
+            int compteur = 0;
+            for (int i = 1; i < note[note.Count - 1].matiere; i++)
+            {
+                moyenneg += moyenneMatiere(i);
+                compteur++;
+            }
+            return Math.Round(moyenneg / compteur, 2);
+        }
+    }
+}
